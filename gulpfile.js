03d33692c1490a0fd94e0bf7507d168e0d53a6d2
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2017-01-04 05:35:27 +0500

    fix(gulpfile): fix stylus builder

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,12 @@ const path          = require('path');
 const gulp          = require('gulp');
 const $             = require('gulp-load-plugins')();
 
+const stylus        = require('stylus');
+const autoprefixer  = require('autoprefixer-stylus');
+
 const del           = require('del');
 const yaml          = require('js-yaml');
+const merge         = require('lodash.merge');
 
 /** Helps */
 function onError(err) {
@@ -44,7 +48,32 @@ gulp.task('build:templates', () => {
     }))
     .pipe($.pug({
       pretty: true,
-      locals: locals
+      locals: merge({
+        Date: Date
+      }, locals),
+      filters: {
+        stylus: function (src, options) {
+          let result = '';
+
+          stylus(src)
+            .define('urlBase64', stylus.url({
+              limit: false,
+              paths: [ __dirname ]
+            }))
+            .use(autoprefixer({
+              browsers: [ 'last 100 version' ]
+            }))
+            .render((err, res) => {
+              if (err) {
+                throw err;
+              }
+
+              result = res;
+            })
+
+          return result;
+        }
+      }
     }))
     .pipe(gulp.dest(paths.templates.to));
 });
