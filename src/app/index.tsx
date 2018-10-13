4cf6d400d8926ee25782936a60c2b780ed0b189b
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2018-10-14 02:06:44 +0300

    chore(*): updated copyright

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 /*!
- * Copyright (C) 2017 SuperPaintman
+ * Copyright (C) 2017-2018 SuperPaintman
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
