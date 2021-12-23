fe7768135e9cb9b0cbd1dd2b416d49d468f6aae0
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2021-12-24 02:28:54 +0300

    feat(pages): optimize blur

--- src/pages/LinksPage.svelte
+++ src/pages/LinksPage.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import Header from '~/components/Header.svelte';
   import Footer from '~/components/Footer.svelte';
 
@@ -49,9 +50,172 @@
       title: 'SuperPaintmanDeveloper@gmail.com'
     }
   ];
+
+  function hexToRgb(v: number): string {
+    const r = (v >> 16) & 0xff;
+    const g = (v >> 8) & 0xff;
+    const b = v & 0xff;
+
+    return `${r}, ${g}, ${b}`;
+  }
+
+  // See: https://medium.com/airbnb-engineering/css-box-shadow-can-slow-down-scrolling-d8ea47ec6867.
+  function renderBlurBox(elem: HTMLDivElement) {
+    const width = elem.offsetWidth;
+    const height = elem.offsetHeight;
+
+    type Blur = {
+      top?: number;
+      bottom?: number;
+      left?: number;
+      right?: number;
+      color?: number;
+      opacity?: number;
+    };
+
+    const blurRadius = 128 * 2;
+    const blurOpacity = 0.03;
+
+    const blurs: Blur[] = [
+      {
+        top: 16,
+        left: 16 * 4,
+        color: 0x00aaff,
+        opacity: blurOpacity
+      },
+      {
+        bottom: -16,
+        right: 16 * 16,
+        color: 0x6b24ff,
+        opacity: blurOpacity
+      },
+      {
+        bottom: 16,
+        right: 16 * 5,
+        color: 0xff0044,
+        opacity: blurOpacity
+      }
+    ];
+
+    let maxX: number | undefined;
+    let minX: number | undefined;
+    let maxY: number | undefined;
+    let minY: number | undefined;
+
+    for (const blur of blurs) {
+      if (blur.left !== undefined) {
+        const v = blur.left;
+        if (maxX === undefined || v > maxX) {
+          maxX = v;
+        }
+        if (minX === undefined || v < minX) {
+          minX = v;
+        }
+      }
+
+      if (blur.right !== undefined) {
+        const v = width - blur.right;
+        if (maxX === undefined || v > maxX) {
+          maxX = v;
+        }
+        if (minX === undefined || v < minX) {
+          minX = v;
+        }
+      }
+
+      if (blur.top !== undefined) {
+        const v = blur.top;
+        if (maxY === undefined || v > maxY) {
+          maxY = v;
+        }
+        if (minY === undefined || v < minY) {
+          minY = v;
+        }
+      }
+
+      if (blur.bottom !== undefined) {
+        const v = height - blur.bottom;
+        if (maxY === undefined || v > maxY) {
+          maxY = v;
+        }
+        if (minY === undefined || v < minY) {
+          minY = v;
+        }
+      }
+    }
+
+    const canvasWidth = (maxX || 0) - (minX || 0) + blurRadius * 2;
+    const canvasHeight = (maxY || 0) - (minY || 0) + blurRadius * 2;
+    const canvasOffsetX = -1 * (blurRadius - (minX || 0));
+    const canvasOffsetY = -1 * (blurRadius - (minY || 0));
+
+    // Draw blurs.
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    if (ctx === null) {
+      return;
+    }
+
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+    for (const blur of blurs) {
+      let x = 0;
+      let y = 0;
+      if (blur.left !== undefined) {
+        x = blur.left;
+      }
+      if (blur.right !== undefined) {
+        x = width - blur.right;
+      }
+      x += blurRadius - (minX || 0);
+      if (blur.top !== undefined) {
+        y = blur.top;
+      }
+      if (blur.bottom !== undefined) {
+        y = height - blur.bottom;
+      }
+      y += blurRadius - (minY || 0);
+
+      const gradient = ctx.createRadialGradient(x, y, 0, x, y, blurRadius);
+
+      if (blur.color !== undefined) {
+        const opacity = blur.opacity || 0.15;
+        gradient.addColorStop(0, `rgba(${hexToRgb(blur.color)}, ${opacity})`);
+        gradient.addColorStop(1, `rgba(${hexToRgb(blur.color)}, 0)`);
+
+        ctx.fillStyle = gradient;
+      }
+
+      ctx.moveTo(x, y);
+      ctx.arc(x, y, blurRadius, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+
+    // Insert blur.
+    const data = canvas.toDataURL();
+
+    const div = document.createElement('div');
+    div.className = elem.className;
+    elem.replaceWith(div);
+
+    const inner = document.createElement('div');
+    inner.style.position = 'absolute';
+    inner.style.top = `${canvasOffsetY}px`;
+    inner.style.left = `${canvasOffsetX}px`;
+    inner.style.width = `${canvasWidth}px`;
+    inner.style.height = `${canvasHeight}px`;
+    inner.style.backgroundImage = `url(${data})`;
+
+    div.appendChild(inner);
+  }
+
+  function setupBlurBox(elem: HTMLDivElement) {
+    renderBlurBox(elem);
+  }
 </script>
 
-<div>
+<div class="root">
   <Header />
 
   <main>
@@ -79,7 +243,7 @@
           {#each links as { name, title, url } (name)}
             <li>
               <a class="link" href={url} {title} target="_table">
-                <div class="blur-outer-box">
+                <div class="blur-outer-box" use:setupBlurBox>
                   <div class="blur blur-1" />
                   <div class="blur blur-2" />
                   <div class="blur blur-3" />
@@ -112,15 +276,19 @@
   $blur-base-size = 2px;
   $blur-radius = 128px;
   // $blur-radius = 16px;
-  $blur-color-1 = rgba(#ff0044, 0.1);
+  $blur-color-1 = rgba(#00aaff, 0.1);
   $blur-color-2 = rgba(#6b24ff, 0.1);
-  $blur-color-3 = rgba(#00aaff, 0.1);
-  $blur-outer-color-3 = rgba(#ff0044, 0.03);
-  $blur-outer-color-2 = rgba(#6b24ff, 0.03);
+  $blur-color-3 = rgba(#ff0044, 0.1);
   $blur-outer-color-1 = rgba(#00aaff, 0.03);
+  $blur-outer-color-2 = rgba(#6b24ff, 0.03);
+  $blur-outer-color-3 = rgba(#ff0044, 0.03);
   // $blur-color-1 = rgba(#ff0044, 0.5);
   // $blur-color-2 = rgba(#6b24ff, 0.5);
   // $blur-color-3 = rgba(#00aaff, 0.5);
+
+  .root {
+    overflow: hidden;
+  }
 
   .container {
     max-width: 1200px;
