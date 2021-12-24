477f52fbf5098780792a6b5b9f49887310c37375
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2021-12-24 21:14:56 +0300

    feat(components): add LinkItem

---
+++ src/components/LinkItem.svelte
@@ -0,0 +1,183 @@
+<script lang="ts">
+  // import { protectEmailAlways } from 'protect-email';
+  import Splash from '~/components/Splash.svelte';
+
+  export let name: string;
+  export let url: string;
+  export let title: string;
+  export let splashOffsetX: number = 0;
+  export let isEmail = false;
+
+  type SplashType = {
+    top?: number;
+    bottom?: number;
+    left?: number;
+    right?: number;
+    color?: number;
+    opacity?: number;
+  };
+
+  const linkSplashOpacity = 0.03;
+  const linkSplashRadius = 128 * 1.5;
+  const linkSplashes: SplashType[] = [
+    {
+      top: 16,
+      left: 16 * 4,
+      color: 0x00aaff,
+      opacity: linkSplashOpacity
+    },
+    {
+      top: 48 + 16,
+      // bottom: -16,
+      right: 16 * 16,
+      color: 0x6b24ff,
+      opacity: linkSplashOpacity
+    },
+    {
+      top: 48 - 16,
+      // bottom: 16,
+      right: 16 * 5,
+      color: 0xff0044,
+      opacity: linkSplashOpacity
+    }
+  ];
+  const linkSplashesActive = linkSplashes.map((splash) => ({
+    ...splash,
+    opacity: 0.06
+  }));
+  const linkSplashesBorder = linkSplashes.map((splash) => {
+    const res = {
+      ...splash,
+      opacity: 0.25
+    };
+
+    if (splash.top) {
+      res.top = splash.top + 2;
+    }
+    if (splash.bottom) {
+      res.bottom = splash.bottom + 2;
+    }
+    if (splash.left) {
+      res.left = splash.left + 2;
+    }
+    if (splash.right) {
+      res.right = splash.right + 2;
+    }
+
+    return res;
+  });
+
+  const href = isEmail ? `mailto:${url}` : url;
+</script>
+
+<svelte:head>
+  {#if !isEmail}
+    <link rel="prefetch" href={url} />
+  {/if}
+</svelte:head>
+
+<a class="root" {href} {title} target={isEmail ? undefined : '_blank'}>
+  <div class="splash" style={`left: ${splashOffsetX}px`}>
+    <Splash splashes={linkSplashes} radius={linkSplashRadius} resizable />
+
+    <div class="active">
+      <Splash
+        splashes={linkSplashesActive}
+        radius={linkSplashRadius}
+        resizable
+      />
+    </div>
+  </div>
+
+  <div class="border">
+    <Splash splashes={linkSplashesBorder} radius={linkSplashRadius} resizable />
+  </div>
+
+  <div class="inner">
+    <div>{name}</div>
+  </div>
+</a>
+
+<style lang="stylus">
+  $border = 2px;
+
+  .root {
+    display: block;
+    position: relative;
+
+    margin-bottom: 16px;
+
+    border-radius: 4px;
+
+    color: inherit;
+    font-size: 16px;
+    line-height: 16px;
+    text-decoration: none;
+  }
+
+  .inner {
+    position: relative;
+
+    padding: 16px 32px;
+
+    background-color: #ffffff;
+
+    overflow: hidden;
+  }
+
+  .border {
+    position: absolute;
+
+    top: -1 * $border;
+    left: -1 * $border;
+
+    width: 100%;
+    height: 100%;
+
+    padding: $border;
+
+    border-radius: 4px;
+
+    overflow: hidden;
+
+    opacity: 0.3;
+
+    transition: opacity 0.03s linear;
+  }
+
+  .root:hover .border,
+  .root:focus .border {
+    opacity: 1;
+  }
+
+  .splash {
+    position: absolute;
+
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 100%;
+
+    z-index: -1;
+  }
+
+  .active {
+    position: absolute;
+
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 100%;
+
+    opacity: 0;
+
+    transition: opacity 0.25s linear;
+  }
+
+  .root:hover .active,
+  .root:focus .active {
+    opacity: 1;
+  }
+</style>
--- src/pages/LinksPage.svelte
+++ src/pages/LinksPage.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import Splash from '~/components/Splash.svelte';
+  import LinkItem from '~/components/LinkItem.svelte';
 
   const src: string = require(`~/images/avatar.jpg?{
     sizes: [256, 128]
@@ -16,105 +16,46 @@
     {
       type: 'github',
       name: 'GitHub',
-      url: '#',
+      url: 'https://github.com/SuperPaintman/',
       title: 'GitHub'
     },
     {
       type: 'linkedin',
       name: 'LinkedIn',
-      url: '#',
+      url: 'https://www.linkedin.com/in/superpaintman/',
       title: 'LinkedIn'
     },
     {
       type: 'telegram',
       name: 'Telegram',
-      url: '#',
+      url: 'https://t.me/superpaintman',
       title: 'Telegram'
     },
     {
       type: 'habr',
       name: 'Habr',
-      url: '#',
+      url: 'https://habr.com/ru/users/superpaintman/',
       title: 'Habr'
     },
     {
       type: 'medium',
       name: 'Medium',
-      url: '#',
+      url: 'https://medium.com/@SuperPaintman',
       title: 'Medium'
     },
     {
       type: 'instagram',
       name: 'Instagram',
-      url: '#',
+      url: 'https://www.instagram.com/superpaintman/',
       title: 'Instagram'
     },
     {
       type: 'email',
       name: 'SuperPaintmanDeveloper@gmail.com',
-      url: '#',
+      url: 'SuperPaintmanDeveloper@gmail.com',
       title: 'Email'
     }
   ];
-
-  type SplashType = {
-    top?: number;
-    bottom?: number;
-    left?: number;
-    right?: number;
-    color?: number;
-    opacity?: number;
-  };
-
-  const linkSplashOpacity = 0.03;
-  const linkSplashRadius = 128 * 1.5;
-  const linkSplashes: SplashType[] = [
-    {
-      top: 16,
-      left: 16 * 4,
-      color: 0x00aaff,
-      opacity: linkSplashOpacity
-    },
-    {
-      top: 48 + 16,
-      // bottom: -16,
-      right: 16 * 16,
-      color: 0x6b24ff,
-      opacity: linkSplashOpacity
-    },
-    {
-      top: 48 - 16,
-      // bottom: 16,
-      right: 16 * 5,
-      color: 0xff0044,
-      opacity: linkSplashOpacity
-    }
-  ];
-  const linkSplashesActive = linkSplashes.map((splash) => ({
-    ...splash,
-    opacity: 0.06
-  }));
-  const linkSplashesBorder = linkSplashes.map((splash) => {
-    const res = {
-      ...splash,
-      opacity: 0.25
-    };
-
-    if (splash.top) {
-      res.top = splash.top + 2;
-    }
-    if (splash.bottom) {
-      res.bottom = splash.bottom + 2;
-    }
-    if (splash.left) {
-      res.left = splash.left + 2;
-    }
-    if (splash.right) {
-      res.right = splash.right + 2;
-    }
-
-    return res;
-  });
 
   function linkSplashOffsetX(i: number, length: number): number {
     if (length <= 1) {
@@ -153,38 +94,13 @@
       <ul class="links">
         {#each links as { type, name, title, url }, i (type)}
           <li>
-            <a class="link" href={url} {title} target="_table">
-              <div
-                class="link-splash"
-                style={`left: ${linkSplashOffsetX(i, links.length)}px`}
-              >
-                <Splash
-                  splashes={linkSplashes}
-                  radius={linkSplashRadius}
-                  resizable
-                />
-
-                <div class="link-splash-active">
-                  <Splash
-                    splashes={linkSplashesActive}
-                    radius={linkSplashRadius}
-                    resizable
-                  />
-                </div>
-              </div>
-
-              <div class="link-border">
-                <Splash
-                  splashes={linkSplashesBorder}
-                  radius={linkSplashRadius}
-                  resizable
-                />
-              </div>
-
-              <div class="inner">
-                <div>{name}</div>
-              </div>
-            </a>
+            <LinkItem
+              {name}
+              {title}
+              {url}
+              splashOffsetX={linkSplashOffsetX(i, links.length)}
+              isEmail={type === 'email'}
+            />
           </li>
         {/each}
       </ul>
@@ -195,7 +111,6 @@
 <style lang="stylus">
   $photo-size = 128px;
   $photo-border-padding = 4px;
-  $link-border = 2px;
 
   .container {
     max-width: 1200px;
@@ -282,85 +197,5 @@
     margin: 64px 0;
 
     list-style: none;
-  }
-
-  .link {
-    display: block;
-    position: relative;
-
-    margin-bottom: 16px;
-
-    border-radius: 4px;
-
-    color: inherit;
-    font-size: 16px;
-    line-height: 16px;
-    text-decoration: none;
-  }
-
-  .link .inner {
-    position: relative;
-
-    padding: 16px 32px;
-
-    background-color: #ffffff;
-
-    overflow: hidden;
-  }
-
-  .link-border {
-    position: absolute;
-
-    top: -1 * $link-border;
-    left: -1 * $link-border;
-
-    width: 100%;
-    height: 100%;
-
-    padding: $link-border;
-
-    border-radius: 4px;
-
-    overflow: hidden;
-
-    opacity: 0.3;
-
-    transition: opacity 0.03s linear;
-  }
-
-  .link:hover .link-border,
-  .link:focus .link-border {
-    opacity: 1;
-  }
-
-  .link-splash {
-    position: absolute;
-
-    top: 0;
-    left: 0;
-
-    width: 100%;
-    height: 100%;
-
-    z-index: -1;
-  }
-
-  .link-splash-active {
-    position: absolute;
-
-    top: 0;
-    left: 0;
-
-    width: 100%;
-    height: 100%;
-
-    opacity: 0;
-
-    transition: opacity 0.25s linear;
-  }
-
-  .link:hover .link-splash-active,
-  .link:focus .link-splash-active {
-    opacity: 1;
   }
 </style>
